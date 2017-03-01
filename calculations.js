4bcd56522cb743682222bc26f7cfb5d7c99e5191
Stacy Zhao <stacyzhao@hotmail.com>
2017-03-01 18:55:54 -0500

    added more test cases regarding values that don't work

--- calculations.js
+++ calculations.js
@@ -10,14 +10,34 @@ var materials = {
 };
 
 module.exports = {
+  
+  // check if price's first value has $, if so, remove and convert it to float
   calculateTotal: function (price, people, material) {
+    if (typeof price != 'number'){
+      price = parseFloat(price.slice(1, price.length));
+    }
     var markupPrice = price * (mandatoryMarkup.markup/100) + price;
-    var peoplePrice = markupPrice * ((mandatoryMarkup.people * people) / 100);
-    if (material in materials) {
-      materialPrice = markupPrice * (materials.food/100);
-    } else {
-      materialPrice = 0;
-    };
+    var materialPrice = module.exports.employeePrice(people, markupPrice);
+    var peoplePrice = module.exports.materialPrice(material, markupPrice);
     return (markupPrice + peoplePrice + materialPrice).toFixed(2);
   },
+
+  // check if material is in the materials object
+  materialPrice: function (material, markupPrice){
+    if (material in materials) {
+      return markupPrice * (materials[material]/100);
+    } else {
+      return 0;
+    };
+  },
+
+  // check if the number of people is a number
+  employeePrice: function (people, markupPrice){
+    if (typeof people === 'number'){
+      return markupPrice * ((mandatoryMarkup.people * people) / 100);
+    }
+  }
+
 };
+
+console.log(module.exports.calculateTotal(5432.00, 1, 'drug'));
--- test/test.js
+++ test/test.js
@@ -6,7 +6,22 @@ describe('calculateTotal', function() {
   it('should return total when the value is not in materials', function() {
     assert.equal(6451.20, calculateTotal(6000.00, 2, 'gears'));
   });
-  it('should return total when the value is available in materials', function() {
+  it('should return total when the value is available in materials (food)', function() {
     assert.equal(1591.58, calculateTotal(1299.99, 3, 'food'));
+  });
+  it('should return total when the value is available in materials (drugs)', function() {
+    assert.equal(6199.81, calculateTotal(5432.00, 1, 'drug'));
+  });
+  it('should return total when the value is available in materials (books)', function() {
+    assert.equal(13707.63, calculateTotal(12456.95, 4, 'books'));
+  });
+  it('should return total when the price starts with $', function() {
+    assert.equal(13707.63, calculateTotal('$12456.95', 4, 'books'));
+  });
+  it('should return NaN when the price is not a number', function() {
+    assert.equal('NaN', calculateTotal('asdf', 4, 'books'));
+  });
+  it('should return NaN when the number of people is not a number', function() {
+    assert.equal('NaN', calculateTotal(12456.95, 's', 'books'));
   });
 });
