b866ee2f0697c67082e7a7d0ea6dd61f4f38e909
Stacy Zhao <stacyzhao@hotmail.com>
2017-03-14 18:22:16 -0400

    assigned test into its own variable

--- test/test.js
+++ test/test.js
@@ -8,8 +8,9 @@ describe('calculateTotal', function() {
   it('should return total when the value is not in materials', function() {
     var price = 1299.99,
         people = 3,
-        material = 'gears',
-        test = calculateTotal(price, people, material);
+        material = 'gears';
+
+    var test = calculateTotal(price, people, material);
 
     expect(test).to.equal(1414.13);
   });
@@ -17,8 +18,9 @@ describe('calculateTotal', function() {
   it('should return total when the value is available in materials', function() {
     var price = 1299.99,
         people = 3,
-        material = 'food',
-        test = calculateTotal(price, people, material);
+        material = 'food';
+
+    var test = calculateTotal(price, people, material);
 
     expect(test).to.equal(1591.58);
   });
@@ -26,16 +28,18 @@ describe('calculateTotal', function() {
   it('should return total when the number of people is 0', function() {
     var price = 5432.00,
         people = 0,
-        material = 'drug',
-        test = calculateTotal(price, people, material);
+        material = 'drug';
+
+    var test = calculateTotal(price, people, material);
 
     expect(test).to.equal(6199.81);
   });
 
   it('should return total when there are more than one or more type of material (array)', function() {
     var price = 1299.99,
-        people = 3,
-        test1 = calculateTotal(price, people, []),
+        people = 3;
+
+    var test1 = calculateTotal(price, people, []),
         test2 = calculateTotal(price, people, ['food']),
         test3 = calculateTotal(price, people, ['food', 'drug', 'service']);
 
@@ -56,8 +60,9 @@ describe('calculateTotal', function() {
 
   it('should return total when the labour count has an invalid input', function() {
     var price = 5432.00,
-        material = 'drug',
-        test1 = calculateTotal(price, "s", material),
+        material = 'drug';
+
+    var test1 = calculateTotal(price, "s", material),
         test2 = calculateTotal(price, [], material),
         test3 = calculateTotal(price, -2, material);
 
