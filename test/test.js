a9ef2da6ab30f94355de39b0d1efcca932fdd3a6
Cam Woloshyn <camw@nulogy.com>
2017-03-20 14:28:34 -0400

    Applies 3A rule to tests

--- test/test.js
+++ test/test.js
@@ -4,8 +4,17 @@ var expect = require('chai').expect;
 var calculateTotal = app.calculateTotal;
 
 describe('calculateTotal', function() {
+  it('calculates the total prices when all inputs are valid', function() {
+    var basePrice = 1299.99,
+      people = 3,
+      material = 'food';
 
-  it('should return total when the value is not in materials', function() {
+    var totalPrice = calculateTotal(basePrice, people, material);
+
+    expect(totalPrice).to.equal(1591.58);
+  });
+
+  it('calculates the total price when material input has no associated markup', function() {
     var basePrice = 1299.99,
         people = 3,
         material = 'gears';
@@ -15,17 +24,7 @@ describe('calculateTotal', function() {
     expect(totalPrice).to.equal(1414.13);
   });
 
-  it('should return total when the value is available in materials', function() {
-    var basePrice = 1299.99,
-        people = 3,
-        material = 'food';
-
-    var totalPrice = calculateTotal(basePrice, people, material);
-
-    expect(totalPrice).to.equal(1591.58);
-  });
-
-  it('should return total when the number of people is 0', function() {
+  it('calculates the total price when no people are assigned to the project', function() {
     var basePrice = 5432.00,
         people = 0,
         material = 'drug';
@@ -35,18 +34,38 @@ describe('calculateTotal', function() {
     expect(totalPrice).to.equal(6199.81);
   });
 
-  it('should return total when there are more than one or more type of material (array)', function() {
+  it('calculates the total price when no materials are specified', function() {
     var basePrice = 1299.99,
-        people = 3;
+        people = 3,
+        material = [];
 
-    var totalPrice1 = calculateTotal(basePrice, people, []),
-        totalPrice2 = calculateTotal(basePrice, people, ['food']),
-        totalPrice3 = calculateTotal(basePrice, people, ['food', 'drug', 'service']);
+    var totalPrice = calculateTotal(basePrice, people, material);
 
-    expect(totalPrice1).to.equal(1414.13);
-    expect(totalPrice2).to.equal(1591.58);
-    expect(totalPrice3).to.equal(1693.95);
+    expect(totalPrice).to.equal(1414.13);
   });
+
+  it('calculates the total price when multiple materials are specified', function() {
+    var basePrice = 1299.99,
+      people = 3,
+      material = ['food', 'drug', 'service'];
+
+    var totalPrice = calculateTotal(basePrice, people, material);
+
+    expect(totalPrice).to.equal(1693.95);
+  });
+
+  /////////
+  // You can do something similar for the tests below, that is, ensure that there
+  // is only a single assertion per test case.
+  //
+  // This may seem like the code is worse, given so much duplication, but that's okay.
+  // A clean test is one where all the necessary information to understand the test case
+  // is included within the example. So in the setup (i.e. the "arrange" part of the test),
+  // you should specify everything you need to establish the state of the test case. Then,
+  // there should be one "act", following which, a single "assert".
+  //
+  // (There are exceptions to the 3A rule, but it's a powerful guide when you're learning TDD.
+  ///
 
   it('should return total when the price has an invalid input', function() {
     var people = 4,
