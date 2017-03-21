edd98315757a916511f3f8b0cf77c24506071c31
Stacy Zhao <stacyzhao@hotmail.com>
2017-03-21 00:07:15 -0400

    Refactored the rest of the tests to reflect 3A rules

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var calculateTotal = app.calculateTotal;
 
 describe('calculateTotal', function() {
-  it('calculates the total prices when all inputs are valid', function() {
+  it('calculates the total price when all inputs are valid', function() {
     var basePrice = 1299.99,
       people = 3,
       material = 'food';
@@ -16,8 +16,8 @@ describe('calculateTotal', function() {
 
   it('calculates the total price when material input has no associated markup', function() {
     var basePrice = 1299.99,
-        people = 3,
-        material = 'gears';
+      people = 3,
+      material = 'gears';
 
     var totalPrice = calculateTotal(basePrice, people, material);
 
@@ -26,8 +26,8 @@ describe('calculateTotal', function() {
 
   it('calculates the total price when no people are assigned to the project', function() {
     var basePrice = 5432.00,
-        people = 0,
-        material = 'drug';
+      people = 0,
+      material = 'drug';
 
     var totalPrice = calculateTotal(basePrice, people, material);
 
@@ -36,8 +36,8 @@ describe('calculateTotal', function() {
 
   it('calculates the total price when no materials are specified', function() {
     var basePrice = 1299.99,
-        people = 3,
-        material = [];
+      people = 3,
+      material = [];
 
     var totalPrice = calculateTotal(basePrice, people, material);
 
@@ -54,40 +54,64 @@ describe('calculateTotal', function() {
     expect(totalPrice).to.equal(1693.95);
   });
 
-  /////////
-  // You can do something similar for the tests below, that is, ensure that there
-  // is only a single assertion per test case.
-  //
-  // This may seem like the code is worse, given so much duplication, but that's okay.
-  // A clean test is one where all the necessary information to understand the test case
-  // is included within the example. So in the setup (i.e. the "arrange" part of the test),
-  // you should specify everything you need to establish the state of the test case. Then,
-  // there should be one "act", following which, a single "assert".
-  //
-  // (There are exceptions to the 3A rule, but it's a powerful guide when you're learning TDD.
-  ///
+  it('should return an error when the price is in dollar format', function() {
+    var basePrice = '$12456.95',
+      people = 4,
+      material = 'books';
 
-  it('should return total when the price has an invalid input', function() {
-    var people = 4,
-        material = 'books',
-        err = 'Invalid Input';
+    var totalPrice = function (){calculateTotal(basePrice, people, material)};
 
-    expect(function (){calculateTotal('$12456.95', people, material)}).to.throw(err);
-    expect(function (){calculateTotal(0, people, material)}).to.throw(err);
-    expect(function (){calculateTotal(-40, people, material)}).to.throw(err);
+    expect(totalPrice).to.throw('Invalid Input');
   });
 
-  it('should return total when the labour count has an invalid input', function() {
+  it('should return an error when the price is zero', function() {
+    var basePrice = 0,
+      people = 4,
+      material = 'books';
+
+    var totalPrice = function (){calculateTotal(basePrice, people, material)};
+
+    expect(totalPrice).to.throw('Invalid Input');
+  });
+
+  it('should return an error when the price is a negative number', function() {
+    var basePrice = -40,
+      people = 4,
+      material = 'books';
+
+    var totalPrice = function (){calculateTotal(basePrice, people, material)};
+
+    expect(totalPrice).to.throw('Invalid Input');
+  });
+
+  it('calculates the total price when the labour count is a string', function() {
     var basePrice = 5432.00,
-        material = 'drug';
+      people = "s",
+      material = 'drug';
 
-    var totalPrice1 = calculateTotal(basePrice, "s", material),
-        totalPrice2 = calculateTotal(basePrice, [], material),
-        totalPrice3 = calculateTotal(basePrice, -2, material);
+    var totalPrice = calculateTotal(basePrice, people, material);
 
-    expect(totalPrice1).to.equal(6199.81);
-    expect(totalPrice2).to.equal(6199.81);
-    expect(totalPrice3).to.equal(6199.81);
+    expect(totalPrice).to.equal(6199.81);
+  });
+
+  it('calculates the total price when the labour count is an empty array', function() {
+    var basePrice = 5432.00,
+      people = [],
+      material = 'drug';
+
+    var totalPrice = calculateTotal(basePrice, people, material);
+
+    expect(totalPrice).to.equal(6199.81);
+  });
+
+  it('calculates the total price when the labour count is a negative number', function() {
+    var basePrice = 5432.00,
+      people = -2,
+      material = 'drug';
+
+    var totalPrice = calculateTotal(basePrice, people, material);
+
+    expect(totalPrice).to.equal(6199.81);
   });
 
 });
